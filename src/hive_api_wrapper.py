2ee5e4261d97777a18618ea0e334f37ea7a7aaf8
anufriu <anufriu@gmail.com>
2022-02-06 04:59:21 +0300

    autopep and gitignore

--- src/hive_api_wrapper.py
+++ src/hive_api_wrapper.py
@@ -4,16 +4,17 @@ import sys
 import requests
 from loguru import logger
 
-#logger
-loglevel= os.environ.get("LOGLEVEL", "DEBUG")
+# logger
+loglevel = os.environ.get("LOGLEVEL", "DEBUG")
 logger.remove()
 logger.level("INFO", color="<green>")
 logger.level("DEBUG", color="<magenta>")
 logger.level("WARNING", color="<yellow>")
 logger.level("ERROR", color="<red>")
 logger.add(sink=sys.stdout,
-format="[{time:YYYY-MM-DD at HH:mm:ss}] <level>[{level}]<bold>[{function}] </bold>{message}</level>",
-level=loglevel, backtrace=True, diagnose=True)
+           format="[{time:YYYY-MM-DD at HH:mm:ss}] <level>[{level}]<bold>[{function}] </bold>{message}</level>",
+           level=loglevel, backtrace=True, diagnose=True)
+
 
 class Wrapper():
     ''' класс реализующий обертку над requests
@@ -24,28 +25,31 @@ class Wrapper():
         - headers: {'accept': 'application/json',  'Authorization': 'Bearer $accessToken'}
     '''
 
-
-    def __init__(self, token:str):
+    def __init__(self, token: str):
         self.token = token
         self.headers = {'accept': 'application/json',
-            'Content-Type': 'application/json', 'Authorization': f'{token}' }
+                        'Content-Type': 'application/json', 'Authorization': f'{token}'}
         self.main_url = 'https://api2.hiveos.farm/api/v2/'
- 
-    def h_req_preset(self, endpoint:str, req_type:str="get", payload:dict={}):
+
+    def h_req_preset(self, endpoint: str, req_type: str = "get", payload: dict = {}):
         '''готовим обьект requests'''
-        logger.info(f'processing endpoint {endpoint} with request type: {req_type}')
+        logger.info(
+            f'processing endpoint {endpoint} with request type: {req_type}')
         try:
             url_and_endpoint = str(self.main_url+endpoint)
-            logger.debug(f'URL: {url_and_endpoint}, request type: {req_type}, payload: {payload}')
+            logger.debug(
+                f'URL: {url_and_endpoint}, request type: {req_type}, payload: {payload}')
             if req_type == "get":
                 req = requests.get(url_and_endpoint, headers=self.headers)
             elif req_type == "post":
-                req = requests.post(url_and_endpoint, headers=self.headers, json=payload)
+                req = requests.post(
+                    url_and_endpoint, headers=self.headers, json=payload)
             if req.status_code in [200, 201]:
                 logger.info('status code 200 all ok')
                 res = req
             else:
-                logger.error(f'status code is not ok: {req.status_code} with message: {req.text}')
+                logger.error(
+                    f'status code is not ok: {req.status_code} with message: {req.text}')
                 res = None
             logger.debug(f'request result: {res}')
         except requests.exceptions.RequestException as e:
@@ -53,9 +57,7 @@ class Wrapper():
             res = None
         return res
 
-
-
-    def h_get_farms_ids(self) ->list:
+    def h_get_farms_ids(self) -> list:
         '''get worker list'''
         data = self.h_req_preset(endpoint='farms', req_type='get')
         if data:
@@ -63,38 +65,40 @@ class Wrapper():
             logger.info(f'got farms ids list {ids}')
             return ids
 
-    def h_get_workers_ids(self, farms) ->dict:
+    def h_get_workers_ids(self, farms) -> dict:
         '''get farm\worker dict {farm_id: [{worker_name: worker_id}]} '''
         farm_ids = farms
         if not farms:
             logger.error('Cant get info about farms')
             return None
         for farm_id in farm_ids:
-            data = self.h_req_preset(endpoint=f'farms/{farm_id}/workers2', req_type='get')
+            data = self.h_req_preset(
+                endpoint=f'farms/{farm_id}/workers2', req_type='get')
             if data:
-                worker_ids = { farm_id:[{worker['name']:worker['id']}
-                            for worker in data.json().get('data')]}
+                worker_ids = {farm_id: [{worker['name']:worker['id']}
+                                        for worker in data.json().get('data')]}
                 logger.info(f'got farm ids list {worker_ids}')
             else:
                 logger.error(f'got empty Data... returning None')
                 return None
         return worker_ids
-    
+
     def h_post_command_execute(self, command, worker_name):
         '''execute command in hiveos'''
         logger.info(f'trying to execute command: {command}..')
-        farms_and_workers= self.h_get_workers_ids()
+        farms_and_workers = self.h_get_workers_ids()
         for farm, worker in farms_and_workers.items():
             worker = worker[0]
             if worker_name in worker.keys():
                 try:
-                    req = self.h_req_preset(endpoint=
-                        f'farms/{farm}/workers/{worker[worker_name]}/command',
-                        req_type="post", payload={"command": "exec", "data": {"cmd":f"{command}"}})
+                    req = self.h_req_preset(endpoint=f'farms/{farm}/workers/{worker[worker_name]}/command',
+                                            req_type="post", payload={"command": "exec", "data": {"cmd": f"{command}"}})
                 except AttributeError:
-                    logger.error(f'got invalid answer from h_req_preset func recieved {req}')
+                    logger.error(
+                        f'got invalid answer from h_req_preset func recieved {req}')
                     return
-                logger.info(f'executed command: {command} with result: {req.json()}')
+                logger.info(
+                    f'executed command: {command} with result: {req.json()}')
             else:
                 logger.info('worker {worker_name} not found or not in list')
 
@@ -105,15 +109,16 @@ class Wrapper():
         logger.info(f'collecting info..')
         for farm in farm_info.keys():
             try:
-                req = self.h_req_preset(endpoint=f'farms/{farm}/workers2', req_type='get')
+                req = self.h_req_preset(
+                    endpoint=f'farms/{farm}/workers2', req_type='get')
                 if req and req.status_code == 200:
                     return req.json()
                 else:
                     return None
             except Exception as e:
-                    logger.error(f'Exception occured while processing: {e}')
+                logger.error(f'Exception occured while processing: {e}')
 
-    def h_get_workers_gpus(self, farm_id, worker_ids:list):
+    def h_get_workers_gpus(self, farm_id, worker_ids: list):
         '''
         get worker gpu info
         '''
@@ -153,7 +158,8 @@ class Wrapper():
         data = {'worker_id': worker_id,
                 'algos': ['ethash']}
         try:
-            req = self.h_req_preset(endpoint=endpoint, req_type='post', payload=data)
+            req = self.h_req_preset(
+                endpoint=endpoint, req_type='post', payload=data)
             if req and req.status_code == 201:
                 res = req.json()
                 logger.info(f'sucsesfully processed with response {res}')
@@ -166,10 +172,10 @@ class Wrapper():
         logger.info('processed')
         return res
 
-    def h_get_worker_hashrate(self, worker)->dict:
+    def h_get_worker_hashrate(self, worker) -> dict:
         '''returns worker hashrate in hash (not mega hash or etc) by coin'''
         try:
-            return { h['coin']:h['hash'] for h in worker['miners_summary']['hashrates']}
+            return {h['coin']: h['hash'] for h in worker['miners_summary']['hashrates']}
         except KeyError:
             logger.error(f'No such keys in json')
             return None
@@ -177,7 +183,7 @@ class Wrapper():
             logger.error(f'invalid worker info type recieved!')
             return None
 
-    def h_get_worker_algo(self, worker)->list:
+    def h_get_worker_algo(self, worker) -> list:
         try:
             return [h['algo'] for h in worker['miners_summary']['hashrates']]
         except KeyError:
@@ -187,7 +193,7 @@ class Wrapper():
             logger.error(f'invalid worker info type recieved!')
             return None
 
-    def h_get_worker_coin(self, worker)->list:
+    def h_get_worker_coin(self, worker) -> list:
         try:
             return [h['coin'] for h in worker['miners_summary']['hashrates']]
         except KeyError:
@@ -197,7 +203,7 @@ class Wrapper():
             logger.error(f'invalid worker info type recieved!')
             return None
 
-    def h_get_worker_power_cons(self, worker)->str:
+    def h_get_worker_power_cons(self, worker) -> str:
         try:
             return worker['stats']['power_draw']
         except KeyError:
@@ -205,9 +211,9 @@ class Wrapper():
             return None
         except TypeError:
             logger.error(f'invalid worker info type recieved!')
-            return None       
+            return None
 
-    def h_get_worker_name(self, worker)->str:
+    def h_get_worker_name(self, worker) -> str:
         try:
             return worker['name']
         except KeyError:
@@ -216,7 +222,3 @@ class Wrapper():
         except TypeError:
             logger.error(f'invalid worker info type recieved!')
             return None
-
-
-
-
--- src/prometheus_metrics.py
+++ src/prometheus_metrics.py
@@ -5,45 +5,52 @@ import sys
 from loguru import logger
 from prometheus_client import Gauge, start_http_server
 
-#logger
-loglevel= os.environ.get("LOGLEVEL", "DEBUG")
+# logger
+loglevel = os.environ.get("LOGLEVEL", "DEBUG")
 logger.remove()
 logger.level("INFO", color="<green>")
 logger.level("DEBUG", color="<magenta>")
 logger.level("WARNING", color="<yellow>")
 logger.level("ERROR", color="<red>")
 logger.add(sink=sys.stdout,
-format="[{time:YYYY-MM-DD at HH:mm:ss}] <level>[{level}]<bold>[{function}]</bold>{message}</level>",
-level=loglevel, backtrace=True, diagnose=True)
+           format="[{time:YYYY-MM-DD at HH:mm:ss}] <level>[{level}]<bold>[{function}]</bold>{message}</level>",
+           level=loglevel, backtrace=True, diagnose=True)
+
 
 class Prom_metrics():
     '''creates http server and send metrics to promethus'''
+
     def __init__(self, port):
         self.http_server = start_http_server(port)
         self.gauge_hourly = Gauge('clear_profitline_hourly', 'farm profit',
-            ['worker_name', 'coin'])
+                                  ['worker_name', 'coin'])
         self.gauge_hashrate = Gauge('worker_hashrate', 'actual worker hashrate',
-            ['worker_name', 'algo'])
+                                    ['worker_name', 'algo'])
         self.gauge_coin_price = Gauge('coin_price', 'actual_coin_price',
-            ['coin'])
-        self.gauge_network_hashrate = Gauge('network_hashrate', 
-                    'current network hasrate', ['coin', 'algo'])
+                                      ['coin'])
+        self.gauge_network_hashrate = Gauge('network_hashrate',
+                                            'current network hasrate', ['coin', 'algo'])
         self.gauge_est_in_coin_profit = Gauge('worker_incoin_profit', 'profit in coin',
-                    ['coin', 'worker'])
+                                              ['coin', 'worker'])
 
     def set_mark(self, metric, labels, metric_name):
         '''set Gauge mark and add labels to metric'''
         try:
             if metric_name == 'clear_profitline_hourly':
-                self.gauge_hourly.labels(worker_name=labels[0], coin=labels[1]).set(metric)
+                self.gauge_hourly.labels(
+                    worker_name=labels[0], coin=labels[1]).set(metric)
             elif metric_name == 'worker_hashrate':
-                self.gauge_hashrate.labels(worker_name=labels[0], algo=labels[1]).set(metric)
+                self.gauge_hashrate.labels(
+                    worker_name=labels[0], algo=labels[1]).set(metric)
             elif metric_name == 'coin_price':
                 self.gauge_coin_price.labels(coin=labels).set(metric)
             elif metric_name == 'network_hashrate':
-                self.gauge_network_hashrate.labels(coin=labels[0], algo=labels[1]).set(metric)
+                self.gauge_network_hashrate.labels(
+                    coin=labels[0], algo=labels[1]).set(metric)
             elif metric_name == 'worker_incoin_profit':
-                self.gauge_est_in_coin_profit.labels(coin=labels[0], worker=labels[1]).set(metric)
-            logger.debug(f'created metric with type {metric_name} and value is: {metric}')
+                self.gauge_est_in_coin_profit.labels(
+                    coin=labels[0], worker=labels[1]).set(metric)
+            logger.debug(
+                f'created metric with type {metric_name} and value is: {metric}')
         except Exception as e:
             logger.error(f'error while setting metric {metric_name}, {e}')
